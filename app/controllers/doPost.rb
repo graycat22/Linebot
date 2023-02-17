a90a8b6e24fbec47b019e5f12272f47e8116f225
EC2 Default User <ec2-user@ip-172-31-6-190.ap-northeast-1.compute.internal>
2023-02-17 22:08:21 +0000

    first commit

---
+++ app/controllers/doPost.rb
@@ -0,0 +1,38 @@
+class LineBotController < ApplicationController
+  protect_from_forgery except: [:callback]
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+
+    head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['channel_secret']
+      config.channel_token = ENV['channel_token']
+    end
+  end
+end
